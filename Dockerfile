abe8bfb258d2d5ef7cf8b78d8fbaa716ef36d113
Oleksandr Medvid <alex.medve@gmail.com>
2024-03-05 11:00:28 +0200

    appp and dev env

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.8-slim
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install --no-cache-dir flask redis
+
+EXPOSE 5000
+
+ENV REDIS_HOST=redis
+
+CMD ["python", "./app.py"]
---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, request
+import redis
+
+app = Flask(__name__)
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+redis_port = os.environ.get('REDIS_PORT', 6379)
+db = redis.Redis(host=redis_host, port=redis_port)
+
+@app.route('/set/<key>', methods=['POST'])
+def set_value(key):
+    """set function"""
+    value = request.get_data(as_text=True)
+    db.set(key, value)
+    return f'Set: {key}'
+
+@app.route('/get/<key>', methods=['GET'])
+def get_value(key):
+    """get function"""
+    value = db.get(key)
+    if value is not None:
+        return 'Get: ' + value.decode('utf-8')
+    else:
+        return 'Get Key not found', 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
